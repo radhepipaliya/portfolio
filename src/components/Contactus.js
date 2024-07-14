796d057549957e0fbc4482de6260ad50152f9c17
~RP <radhepipaliya13112004@gmail.com>
2024-07-14 17:52:05 +0530

    touppercase

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 export default function Contactus(props) {
 
     const handleUpClick=()=>{
-        console.log("UperrCase Clicked "+ text);
-        setText("You have Clicked on HandleUp Click")
+        console.log("UperrCase Clicked " +  text);
+        let newText=text.toUpperCase();
+        setText(newText);
     }
      
 
-    const handleOnchange=(event)=>{ 
-        console.log("Onchange");
+    const handleOnchange= (event)=>{ 
+        console.log("On change");
         setText(event.target.value)
     }
     
@@ -26,10 +27,10 @@ const[text,setText]=useState("Enter text here");
                 <input type="email" className="form-control" id="mybox" placeholder="name@example.com"/>
             </div> */}
             <div className="mb-3">
-                <textarea className="form-control" value={text} onClick={handleOnchange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnchange} id="myBox" rows="8"></textarea>
             </div>
 
-            <button type="button" className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+            <button type="button" className="btn btn-primary" onClick={handleUpClick}> Convert to uppercase</button>
         </div>
         </>
 
